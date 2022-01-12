ab5202dddcc0ca6646496efb700b71f52b5d882f
Jack Reynolds <jack.reynolds@bbc.co.uk>
2022-01-12 14:11:02 +0000

    first draft

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+let firstName = "Jack";
+let lastName = "Reynolds"
+let month = 4;
+let day = 16;
+let canSing = true;
+console.log(firstName + " " + lastName);
+let age = 46;
+age++;
+age--;
+console.log(age);
+let message = "hello ";
+message += firstName;
+console.log(message);
+var a = 3, b = 3.1415;
+console.log(`PI is roughly ${a} or more roughly ${b}`);
+if (age > 18) {
+    console.log("adult");
+    console.log("old enough to vote");
+}
