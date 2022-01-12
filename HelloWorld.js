a57c33f0dedf750a9f576aad24aa6e847865f78f
Jack Reynolds <jack.reynolds@bbc.co.uk>
2022-01-12 14:12:30 +0000

    edited age

--- HelloWorld.js
+++ HelloWorld.js
@@ -5,7 +5,7 @@ let month = 4;
 let day = 16;
 let canSing = true;
 console.log(firstName + " " + lastName);
-let age = 46;
+let age = 18;
 age++;
 age--;
 console.log(age);
@@ -14,7 +14,7 @@ message += firstName;
 console.log(message);
 var a = 3, b = 3.1415;
 console.log(`PI is roughly ${a} or more roughly ${b}`);
-if (age > 18) {
+if (age == 18) {
     console.log("adult");
     console.log("old enough to vote");
 }
